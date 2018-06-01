210eb09c272041f26842721dce110325baac03f3
Mercy <jensen@larkin.space>
2018-06-01 02:34:31 +0100

    Added link to help

--- src/commands/help.js
+++ src/commands/help.js
@@ -9,6 +9,6 @@ module.exports = {
     ]
   },
   execute: (bot, r, msg, args) => {
-    msg.channel.createMessage('Command List: ```' + bot.commands.map((c) => bot.config.prefix + c.info.help.toLowerCase() + ' - ' + c.info.desc).join('\n') + '```');
+    msg.channel.createMessage('Command List: ```' + bot.commands.map((c) => bot.config.prefix + c.info.help.toLowerCase() + ' - ' + c.info.desc).join('\n') + '\nThis is a hosted instance of the discord-tickets bot made by Mercy#0001. Host your own by downloading the source from: https://github.com/MercyDoesCode/discord-tickets/```');
   }
 }
